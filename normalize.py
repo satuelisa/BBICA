62707a22476bd1d48b08bfb48ec0ba57b0e09d5d
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-08-31 09:47:15 -0500

    Now using the blue channel for the normalized difference R - G

--- normalize.py
+++ normalize.py
@@ -8,6 +8,8 @@ def normalize(inputImage, outputImage):
     gLow = 255
     rHigh = 0
     gHigh = 0
+    dLow = 255
+    dHigh = -255
     for x in range(w):
         for y in range(h):
             (r, g, b, a) = pixels[x, y]
@@ -15,16 +17,22 @@ def normalize(inputImage, outputImage):
             rHigh = max(r, rHigh)
             gLow = min(g, gLow)
             gHigh = max(g, gHigh)
+            d = r - g
+            dLow = min(d, dLow)
+            dHigh = max(d, dHigh)
     print(f'Normalizing R from [{rLow}, {rHigh}] to [0, 255]')
     print(f'Normalizing G from [{gLow}, {gHigh}] to [0, 255]')
     rSpan = rHigh - rLow
     gSpan = gHigh - gLow
+    dSpan = dHigh - dLow
     for x in range(w):
         for y in range(h):
             (r, g, b, a) = pixels[x, y]
             rN = int(round(255 * (r - rLow) / rSpan))
             gN = int(round(255 * (g - gLow) / gSpan))
-            pixels[x, y] = (rN, gN, 0, 255)
+            d = (r - g)
+            dN = int(round(255 * (d - dLow) / dHigh))            
+            pixels[x, y] = (rN, gN, dN, 255)
     im.save(outputImage)
 
 for image in ['ex1.png', 'ex2.png']:
